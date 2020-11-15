dd904e5d25082336366b57dd2f6e87fab28ac9cd
chwy007 <394467690@qq.com>
2020-11-15 12:39:02 +0800

    定时任务 quartz

---
+++ src/main/java/com/yujizi/utils/MyJob.java
@@ -0,0 +1,18 @@
+package com.yujizi.utils;
+
+/**
+ * @ProjectName: activiti_demo
+ * @Package: com.yujizi.utils
+ * @ClassName: MyJob
+ * @Author: ychw
+ * @Description:
+ * @Date: 2020/11/14 21:27
+ * @Version: 1.0
+ */
+public class MyJob {
+
+    public void execute(){
+        System.out.println("任务执行中==========================");
+    }
+
+}
